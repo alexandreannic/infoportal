d013b0e23ab3a89e16c0a93534b8342fbc9eae7b
Alexandre Annic <alexandre.annic1@gmail.com>
2024-05-31 11:37:16 +0300

    ok

---
+++ infoportal-server/src/server/controller/ControllerKoboWsUpdates.ts
@@ -0,0 +1,12 @@
+import {GlobalEvent} from '../../core/GlobalEvent'
+
+export class ControllerKoboMeta {
+
+  constructor(
+    private event: GlobalEvent.Class = GlobalEvent.Class.getInstance(),
+  ) {
+
+
+  }
+
+}
