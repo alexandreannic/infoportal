8bbc4d7cee9ddb4d66238c9ec7a4c55e858ad843
Pavlo Boiko <pavlo.boiko@drc.ngo>
2025-01-23 09:57:39 +0200

    clean up: get rid of unused vars

--- packages/infoportal-server/src/feature/ecrec/EcrecDbService.ts
+++ packages/infoportal-server/src/feature/ecrec/EcrecDbService.ts
@@ -4,13 +4,11 @@ import {type ApiPaginate, ApiPaginateHelper, DrcProgram} from 'infoportal-common
 import type {IKoboMeta} from 'infoportal-common/kobo/IKoboMeta'
 
 import {KoboMetaService} from '../kobo/meta/KoboMetaService'
-import {WfpDeduplicationService} from '../wfpDeduplication/WfpDeduplicationService'
 
 export class EcrecDbService {
   constructor(
     private prisma: PrismaClient,
     private meta = new KoboMetaService(prisma),
-    private wfp: WfpDeduplicationService = new WfpDeduplicationService(prisma),
   ) {}
 
   readonly search = async (): Promise<ApiPaginate<IKoboMeta>> => {
