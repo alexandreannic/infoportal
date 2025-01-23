0d37c35fd50427a0ce77b6c2ee28d25c15ab9773
Pavlo Boiko <pavlo.boiko@drc.ngo>
2025-01-23 09:50:28 +0200

    get rid of redundant EcrecCachedDb class

--- packages/infoportal-server/src/server/controller/ControllerEcrec.ts
+++ packages/infoportal-server/src/server/controller/ControllerEcrec.ts
@@ -2,19 +2,14 @@ import type {PrismaClient} from '@prisma/client'
 import type {Request, Response} from 'express'
 
 import {EcrecDbService} from '../../feature/ecrec/EcrecDbService'
-import {EcrecCachedDb} from '../../feature/ecrec/EcrecCachedDb'
+import {app, AppCacheKey} from '../../index'
 
 export class ControllerEcrec {
   constructor(
     private prisma: PrismaClient,
-    private cache: EcrecCachedDb = EcrecCachedDb.constructSingleton(prisma),
+    private cache = app.cache,
     private service: EcrecDbService = new EcrecDbService(prisma),
   ) {}
-
-  readonly refresh = async (_request: Request, response: Response) => {
-    await this.cache.refresh()
-    response.send()
-  }
 
   readonly search = async (req: Request, res: Response) => {
     const data = await this.service.search()
