e296c11c8e2c7fadd4a2da0b7ecb6204256a6f18
Pavlo Boiko <pavlo.boiko@drc.ngo>
2025-01-29 15:55:32 +0200

    update Ecrec_msmeGrantReg types

--- packages/infoportal-scripts/src/index.ts
+++ packages/infoportal-scripts/src/index.ts
@@ -1,6 +1,10 @@
-import {KoboClient} from 'kobo-sdk'
-import {appConf} from './appConf'
 import winston from 'winston'
+
+import {KoboClient} from 'kobo-sdk'
+
+import {appConf} from './appConf'
+// import {BuildKoboType} from './kobo/BuildTypeKobo'
+// import {FixKoboMigration} from './kobo-migration/20250113-fixKoboMigration'
 
 export const koboSdkHumanitarian = new KoboClient({
   urlv1: appConf.kobo.humanitarian.urlV1 + '/api/v1',
@@ -23,4 +27,5 @@ import {BuildKoboType} from './kobo/BuildTypeKobo'
   // await ActivityInfoBuildType.fslc()
   // await new BuildKoboType().build('ecrec_msme_bha388')
   // await new BuildKoboType().build('ecrec_vet2_dmfa')
+  // await new BuildKoboType().build('ecrec_msmeGrantReg')
 })()
