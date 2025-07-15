a04283f8e2d6dea33dab9525a80b097bf1d5972a
Alexandre Annic <alexandre.annic1@gmail.com>
2025-07-15 11:13:37 -0500

    [refacto][client][tanstack-migration] Fix useDefaultTabRedirect

--- packages/infoportal-client/src/features/Form/Form.tsx
+++ packages/infoportal-client/src/features/Form/Form.tsx
@@ -4,8 +4,7 @@ import {useQuerySchema} from '@/core/query/useQuerySchema'
 import {useLayoutContext} from '@/shared/Layout/LayoutContext'
 import {Icon, Tab, Tabs} from '@mui/material'
 import {createContext, useContext, useEffect, useMemo} from 'react'
-// import {useLocation, useOutletContext} from 'react-router'
-import {createRoute, Link, Outlet, useMatches} from '@tanstack/react-router'
+import {createRoute, Link, Outlet, useMatches, useNavigate, useRouterState} from '@tanstack/react-router'
 import {useQueryFormById} from '@/core/query/useQueryForm'
 import {Ip} from 'infoportal-api-sdk'
 import {KoboSchemaHelper} from 'infoportal-common'
@@ -30,19 +29,29 @@ export const formRoute = createRoute({
   component: Form,
 })
 
-export const useDefaultTabRedirect = ({workspaceId, formId}: {workspaceId: Ip.Uuid; formId: Ip.FormId}) => {
-  // const navigate = useNavigate()
-  // const pathname = useRouterState({select: s => s.location.pathname})
-  // const querySchema = useQuerySchema({formId, workspaceId})
-  //
-  // useEffect(() => {
-  //   if (querySchema.isLoading) return
-  //   const expectedRoot = appRouter.ws(workspaceId).form.byId(formId).root
-  //   const toAnswers = appRouter.ws(workspaceId).form.byId(formId).answers
-  //   const toFormCreator = appRouter.ws(workspaceId).form.byId(formId).formCreator
-  //   if (pathname !== expectedRoot) return
-  //   navigate(querySchema.data ? toAnswers : toFormCreator)
-  // }, [pathname, querySchema.isLoading])
+export const useDefaultTabRedirect = ({
+  currentFullPath,
+  workspaceId,
+  formId,
+}: {
+  currentFullPath?: string
+  workspaceId: Ip.Uuid
+  formId: Ip.FormId
+}) => {
+  const navigate = useNavigate()
+  const pathname = useRouterState({select: s => s.location.pathname})
+  const querySchema = useQuerySchema({formId, workspaceId})
+
+  useEffect(() => {
+    if (querySchema.isLoading) return
+    if (currentFullPath == formRoute.fullPath) {
+      if (querySchema.data) {
+        navigate({to: '/$workspaceId/form/$formId/answers', params: {workspaceId, formId}})
+      } else {
+        navigate({to: '/$workspaceId/form/$formId/formCreator', params: {workspaceId, formId}})
+      }
+    }
+  }, [pathname, querySchema.isLoading])
 }
 
 export type FormContext = {
@@ -59,7 +68,7 @@ function Form() {
   const {workspaceId, formId} = formRoute.useParams()
   const {m} = useI18n()
   const {setTitle} = useLayoutContext()
-  const match = useMatches().slice(-1)[0]
+  const currentFullPath = useMatches().slice(-1)[0].fullPath
 
   const querySchema = useQuerySchema({formId, workspaceId})
   const queryForm = useQueryFormById({workspaceId, formId}).get
@@ -81,7 +90,7 @@ function Form() {
       return <Page width="full" loading={true} />
     }
     if (!queryForm.data) {
-      return <>ERROR</>
+      return <>Error</>
     }
 
     return (
@@ -99,7 +108,7 @@ function Form() {
 
   return (
     <Page width="full">
-      <Tabs variant="scrollable" scrollButtons="auto" value={match.fullPath}>
+      <Tabs variant="scrollable" scrollButtons="auto" value={currentFullPath}>
         <Tab
           icon={<Icon>{appConfig.icons.dataTable}</Icon>}
           iconPosition="start"
