90e8821ba44f50b0a399a2c2cb1b621a473dfa18
Alexandre Annic <alexandre.annic1@gmail.com>
2025-11-20 13:42:44 -0500

    [feat][core][Input] tiny size

--- packages/client-core/src/ui/Input.tsx
+++ packages/client-core/src/ui/Input.tsx
@@ -1,11 +1,13 @@
 import {FormControl, FormHelperText, InputLabel, OutlinedInput, OutlinedInputProps, TextFieldProps} from '@mui/material'
-import React, {ReactNode, useEffect, useRef} from 'react'
+import React, {ReactNode, useEffect, useMemo, useRef} from 'react'
 
-export interface InputProps extends OutlinedInputProps, Pick<TextFieldProps, 'InputProps' | 'InputLabelProps'> {
-  label?: string
-  helperText?: ReactNode
-  disableBrowserAutocomplete?: boolean
-}
+export type InputProps = Omit<OutlinedInputProps, 'size'> &
+  Pick<TextFieldProps, 'InputProps' | 'InputLabelProps'> & {
+    label?: string
+    size?: 'tiny' | 'small' | 'medium'
+    helperText?: ReactNode
+    disableBrowserAutocomplete?: boolean
+  }
 
 export const Input = React.forwardRef(
   (
@@ -32,8 +34,10 @@ export const Input = React.forwardRef(
     if (label) label = label + (label && required ? ' *' : '')
     // const notch = !!props.value
 
+    const muiSize = useMemo(() => (size === 'tiny' ? 'small' : size), [size])
+
     return (
-      <FormControl size={size} sx={{width: '100%', ...sx}} error={error} variant="outlined">
+      <FormControl size={muiSize} sx={{width: '100%', ...sx}} error={error} variant="outlined">
         {label && (
           <InputLabel htmlFor={id} {...InputLabelProps}>
             {label}
@@ -44,10 +48,25 @@ export const Input = React.forwardRef(
           label={label}
           inputRef={inputElement}
           required={required}
-          size={size}
+          size={muiSize}
           error={error}
           ref={ref}
           {...props}
+          slotProps={
+            size === 'tiny'
+              ? {
+                  ...props.slotProps,
+                  input: {
+                    ...props.slotProps?.input,
+                    sx: {
+                      ...props.slotProps?.input?.sx,
+                      paddingTop: '6px',
+                      paddingBottom: '6px',
+                    },
+                  },
+                }
+              : {}
+          }
           inputProps={{
             ...props.inputProps,
             ...(disableBrowserAutocomplete && {
